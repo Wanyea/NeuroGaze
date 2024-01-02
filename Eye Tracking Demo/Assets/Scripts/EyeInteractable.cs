7558c0e9d89ecb3cbff0e4a6eacb7a09d33a9413
Wanyea Barbel <wanyea.barbel@knights.ucf.edu>
2024-01-01 19:21:13 -0500

    eye tracking stable

--- Eye Tracking Demo/Assets/Scripts/EyeInteractable.cs
+++ Eye Tracking Demo/Assets/Scripts/EyeInteractable.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
@@ -6,118 +5,19 @@ using UnityEngine;
 public class EyeInteractable : MonoBehaviour
 {
     [SerializeField]
-    private Material OnHoverActiveMaterial;
-    [SerializeField]
-    private Material OnMentalCommandActiveMaterial; 
-    [SerializeField]
-    private MentalCommands mentalCommands;
-    [SerializeField]
-    private float moveSpeed = 1.0f;
+    private Material OnHoverMaterial;
 
     private Material originalMaterial;
     private MeshRenderer meshRenderer;
-    private Transform cameraTransform;
-    private bool isHovered = false;
 
     private void Start()
     {
         meshRenderer = GetComponent<MeshRenderer>();
         originalMaterial = meshRenderer.material;
-        cameraTransform = Camera.main.transform; 
-
-        if (mentalCommands == null)
-        {
-            Debug.LogError("MentalCommands reference not set in EyeInteractable");
-        }
-    }
-
-    private void Update()
-    {
-        if (isHovered) 
-        {
-            Debug.Log("Hoevered over cube!");
-            var mentalCommand = mentalCommands.GetMentalCommand();
-
-            /*// Check if connection with Emotiv has been established and mentals command are coming through
-            if (mentalCommands.GetMentalCommand() != null) 
-            {
-                
-                HandleMentalCommandMovement(mentalCommand);
-                UpdateMaterialBasedOnMentalCommand(mentalCommand);
-            }*/
-
-        }
-    }
-
-    private void HandleMentalCommandMovement(string command)
-    {
-        
-        Vector3 rayOrigin = Camera.main.transform.position; 
-        Vector3 directionFromRayOrigin = (transform.position - rayOrigin).normalized;
-        float step = moveSpeed * Time.deltaTime;
-
-        switch (command)
-        {
-            case "pull":
-            case "push":
-                transform.position += command == "pull" ? -directionFromRayOrigin * step : directionFromRayOrigin * step;
-                break;
-
-            case "neutral":
-            case "":
-                // Stop movement
-                break;
-        }
-    }
-
-    private void UpdateMaterialBasedOnMentalCommand(string command)
-    {
-        if (command == "pull" || command == "push")
-        {
-            meshRenderer.material = OnMentalCommandActiveMaterial;
-        }
-        else
-        {
-            meshRenderer.material = originalMaterial;
-        }
     }
 
     public void Hover(bool state)
     {
-        isHovered = state;
-
-        if (state)
-        {
-            // meshRenderer.material = OnHoverActiveMaterial;
-        }
-        else
-        {
-            // meshRenderer.material = originalMaterial;
-        }
-    }
-
-    private void OnEnable()
-    {
-        if (mentalCommands != null)
-        {
-            mentalCommands.MentalCommandChanged += OnMentalCommandChanged;
-        }
-    }
-
-    private void OnDisable()
-    {
-        if (mentalCommands != null)
-        {
-            mentalCommands.MentalCommandChanged -= OnMentalCommandChanged;
-        }
-    }
-
-    private void OnMentalCommandChanged(string command)
-    {
-        if (isHovered)
-        {
-            HandleMentalCommandMovement(command);
-            UpdateMaterialBasedOnMentalCommand(command);
-        }
+        meshRenderer.material = state ? OnHoverMaterial : originalMaterial;
     }
 }
--- Eye Tracking Demo/Assets/Scripts/EyeTrackingRay.cs
+++ Eye Tracking Demo/Assets/Scripts/EyeTrackingRay.cs
@@ -1,89 +1,68 @@
 using UnityEngine;
-using System.Collections.Generic;
 
 [RequireComponent(typeof(LineRenderer))]
 public class EyeTrackingRay : MonoBehaviour
 {
-    [SerializeField]
-    private float rayDistance = 500.0f;
-
-    [SerializeField]
-    private float rayWidth = 0.01f;
-
-    [SerializeField]
-    private LayerMask layersToInclude;
+    [SerializeField] private float rayDistance = 500.0f;
+    [SerializeField] private Transform leftEyeAnchor;
+    [SerializeField] private Transform rightEyeAnchor;   
 
     private LineRenderer lineRenderer;
-    private Dictionary<int, EyeInteractable> interactables = new Dictionary<int, EyeInteractable>();
     private EyeInteractable lastEyeInteractable;
-    private Transform leftEyeAnchor;
-    private Transform rightEyeAnchor;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
         SetupRay();
-
-        // Get the eye anchors for more accurate raycast/line renderer start position 
-        OVRCameraRig cameraRig = FindObjectOfType<OVRCameraRig>();
-        if (cameraRig)
-        {
-            leftEyeAnchor = cameraRig.leftEyeAnchor;
-            rightEyeAnchor = cameraRig.rightEyeAnchor;
-        }
     }
 
     private void SetupRay()
     {
-        lineRenderer.useWorldSpace = false;
+        lineRenderer.useWorldSpace = true;
         lineRenderer.positionCount = 2;
-        lineRenderer.startWidth = rayWidth;
-        lineRenderer.endWidth = rayWidth;
-        lineRenderer.SetPosition(0, transform.position);
-        lineRenderer.SetPosition(1, new Vector3(transform.position.x, transform.position.y, transform.position.z + rayDistance));
+        lineRenderer.startWidth = 0.01f;
+        lineRenderer.endWidth = 0.01f;
     }
 
     private void Update()
     {
-        if (leftEyeAnchor == null || rightEyeAnchor == null) return;
-
         Vector3 eyesCenter = (leftEyeAnchor.position + rightEyeAnchor.position) * 0.5f;
+        Ray ray = new Ray(eyesCenter, transform.forward);
+        RaycastHit hit;
+        bool isHit = Physics.Raycast(ray, out hit, rayDistance);
 
-        Vector3 rayDirection = transform.TransformDirection(Vector3.forward) * rayDistance;
-        bool intercepting = Physics.Raycast(eyesCenter, rayDirection, out RaycastHit hit, Mathf.Infinity, layersToInclude);
-
-        // Change the LineRenderer start position to the eyes center
-        lineRenderer.SetPosition(0, transform.InverseTransformPoint(eyesCenter));
-
-        if (intercepting)
+        if (isHit)
         {
-            Debug.Log("Starting intercepting...");
-            if (!interactables.TryGetValue(hit.transform.gameObject.GetHashCode(), out EyeInteractable eyeInteractable))
+            EyeInteractable eyeInteractable = hit.collider.GetComponent<EyeInteractable>();
+            if (eyeInteractable)
             {
-                eyeInteractable = hit.transform.GetComponent<EyeInteractable>();
-                interactables.Add(hit.transform.gameObject.GetHashCode(), eyeInteractable);
+                if (lastEyeInteractable != eyeInteractable)
+                {
+                    if (lastEyeInteractable != null)
+                    {
+                        lastEyeInteractable.Hover(false);
+                    }
+                    lastEyeInteractable = eyeInteractable;
+                }
+                eyeInteractable.Hover(true);
             }
-
-            var toLocalSpace = transform.InverseTransformPoint(hit.point); 
-            lineRenderer.SetPosition(1, toLocalSpace); 
-
-            if (lastEyeInteractable && lastEyeInteractable != eyeInteractable)
-            {
-                lastEyeInteractable.Hover(false);
-            }
-
-            eyeInteractable.Hover(true);
-            lastEyeInteractable = eyeInteractable;
-        }
-        else
-        {
-            if (lastEyeInteractable)
+            else if (lastEyeInteractable != null)
             {
                 lastEyeInteractable.Hover(false);
                 lastEyeInteractable = null;
             }
-            lineRenderer.SetPosition(1, new Vector3(0, 0, rayDistance));
         }
-    }
+        else
+        {
+            if (lastEyeInteractable != null)
+            {
+                lastEyeInteractable.Hover(false);
+                lastEyeInteractable = null;
+            }
+        }
 
+        // Update the LineRenderer to represent the ray
+        lineRenderer.SetPosition(0, eyesCenter);
+        lineRenderer.SetPosition(1, eyesCenter + transform.forward * rayDistance);
+    }
 }
